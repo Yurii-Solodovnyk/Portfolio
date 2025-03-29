88578266fbb474336173ad6229dc118e7ec9f194
solodovnyk@qarea.biz <yurii.solodovnyk@gmail.com>
2025-03-29 14:59:18 +0200

    Refactor translation logic by adding i18n

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,18 +10,6 @@ function changeBackground(color) {
     }
 }
 
-function makeBorderBlue() {
-    document.getElementById('page').style.borderColor = 'blue';
-}
-
-function makeBorderGreen() {
-    document.getElementById('page').style.borderColor = 'green';
-}
-
-function makeBorderYellow() {
-    document.getElementById('page').style.borderColor = 'yellow';
-}
-
 window.onload = changeBackground(3);
 
 document.getElementById('likes').onmouseenter = function () {
